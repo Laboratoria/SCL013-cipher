3ffacb8b848f0441d3185fa2b1f5700923ad429b
LoretoGutierrezH <loretocgh@gmail.com>
2020-04-05 15:17:10 -0400

    cipher.js encode/decode almost ready

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,49 @@
 const cipher = {
-  // ...
-};
+  encode: function (offset, string) {
+    //1. Transformar input a mayúsculas
+    string = string.toUpperCase();
+    console.log(string);
+    let encodedString = "";
+    //2. Transformar input de number a string
+    //3. Iterar cada una de las letras del string
+    for(let i = 0; i < string.length; i++) {
+      //console.log(string[i]);
+      //console.log(string.charCodeAt(i));
+    //4. Convertir letra a código y guardarla en x
+      let x = string.charCodeAt(i);
+    //5. Utilizar la fórmula y guardar resultado en x
+      x = (x - 65 + offset) % 26 + 65;
+    //6. Convertir código en letra e ir guardando en encodedString
+      encodedString += String.fromCharCode(x);
+      
+    }
+    return encodedString;
+  },
+  
+  decode: function (offset, string) {
+    //1. Transformar input a mayúsculas
+    string = string.toUpperCase();
+    let decodedString = "";
+    //2. Transformar input de number a string
+    //3. Iterar cada una de las letras del string
+    for (let i = 0; i < string.length; i++) {
+      let x = string.charCodeAt(i);
+      x = (x - 65 - offset) % 26 + 65;
+      console.log(x);
+      if (x === 58) {
+        x = 32;
+      } else if (x === 60) {
+        x = 86;
+      } 
+      decodedString += String.fromCharCode(x);
+      
+    }
+    return decodedString;
+  }
+} 
+
 
 export default cipher;
+
+/* (x - 65 + offset) % 26 + 65; */
+ 
--- src/index.js
+++ src/index.js
@@ -1,3 +1,71 @@
 import cipher from './cipher.js';
 
-console.log(cipher);
+
+//párrafo donde se muestra el código
+const randomCode = document.getElementById("random-code");
+
+//input donde se ingresar el username
+const userName = document.getElementById("username");
+
+//botón que muestra el código
+const codeBtn = document.getElementById("code-btn");
+
+codeBtn.addEventListener("click", giveCodeFn);
+let code;
+
+function giveCodeFn(event) {
+  if (userName.value === "") {
+    alert("Ingresa un nombre de usuario");
+  } else {
+    code = Math.floor((9999 - 1000) * Math.random()) + 1000;
+    randomCode.innerHTML = `${code}`;
+  }
+
+  event.preventDefault()
+
+  
+}
+
+
+
+//botón que ingresa a la app
+const enterBtn = document.getElementById("enter-btn");
+
+enterBtn.addEventListener("click", enterFn);
+
+function enterFn() {
+
+  const content = document.getElementById("content");
+
+  content.innerHTML = "";
+  content.innerHTML = `
+    <div id="cipher-decipher">
+
+      <h4>Escribe tu texto acá</h4>
+      <textarea id="decipher" name="">
+      hola
+      </textarea>
+
+      <h4>Este es el texto cifrado</h4>
+      <textarea id="cipher" name="">
+      lkas
+      </textarea>
+    </div>
+  
+  `
+}
+
+
+
+function cipherFn() {
+  alert("Cifrando mensaje (se supone");
+};
+
+function decipher() {
+  alert("Descifrando mensaje (se supone)");
+};
+
+
+//console.log(cipher);
+console.log(cipher.encode(5, "Hola, nos vemos en ID TELOS A LAS CINCO CUARENTA Y CINCO DE LA TARDE"));
+console.log(cipher.decode(5, "MTQF1?STX?AJRTX?JS?NI?YJQTX?F?QFX?HNSHT?HZFWJSYF?D?HNSHT?IJ?QF?YFWIJ"));

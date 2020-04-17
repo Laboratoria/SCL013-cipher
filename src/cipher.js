e6faafb369ad4cb5fea66b1ae0bcfe7f1b9e2fdd
johanazapata <johanazapata2012@gmail.com>
2020-04-17 09:54:18 -0400

    Actualización

--- src/cipher.js
+++ src/cipher.js
@@ -1,21 +1,21 @@
 export function encode(string, offset) {
-    let codigoAscii = "";
-    let resultado = "";
-    for (let posicion = 0; posicion < (string.length); posicion++) {
-        var letraEnPosicion = string.charCodeAt(posicion);
-        codigoAscii = ((letraEnPosicion - 65 + parseInt(offset)) % 26 + 65);
+  let codigoAscii = "";
+  let resultado = "";
+  for (let posicion = 0; posicion < (string.length); posicion++) {
+    var letraEnPosicion = string.charCodeAt(posicion);
+    codigoAscii = ((letraEnPosicion - 65 + parseInt(offset)) % 26 + 65);
 
-        resultado += String.fromCharCode(codigoAscii);
-    }
-    return resultado;
+    resultado += String.fromCharCode(codigoAscii);
+  }
+  return resultado;
 }
 
 export function decode(string, offset) {
-    let codigoAscii = "";
-    let resultado = "";
-    for (let posicion = 0; posicion < (string.length); posicion++) {
-        codigoAscii = ((string.charCodeAt(posicion) + 65 - parseInt(offset)) % 26 + 65);
-        resultado += String.fromCharCode(codigoAscii);
-    }
-    return resultado;
+  let codigoAscii = "";
+  let resultado = "";
+  for (let posicion = 0; posicion < (string.length); posicion++) {
+    codigoAscii = ((string.charCodeAt(posicion) + 65 - parseInt(offset)) % 26 + 65);
+    resultado += String.fromCharCode(codigoAscii);
+  }
+  return resultado;
 }
--- src/index.js
+++ src/index.js
@@ -1,29 +1,31 @@
-import { encode, decode } from './cipher.js'
+import {
+  encode,
+  decode
+} from './cipher.js'
 
 document.getElementById('Cifrar').addEventListener('click', cifrarMensaje)
 document.getElementById('Descifrar').addEventListener('click', descifrarMensaje)
 
 export function cifrarMensaje() {
 
-    let offset = document.getElementById("desplazar").value;
-    let string = document.getElementById("mensaje_cifrar").value.toUpperCase();
+  let offset = document.getElementById("desplazar").value;
+  let string = document.getElementById("mensaje_cifrar").value.toUpperCase();
 
-    const resultado = encode(string, offset);
-    document.getElementById("mensaje_cifrado").value = resultado;
+  const resultado = encode(string, offset);
+  document.getElementById("mensaje_cifrado").value = resultado;
 }
 
 export function descifrarMensaje() {
-    let offset = document.getElementById("desplazar").value;
-    let string = document.getElementById("mensaje_cifrar").value.toUpperCase();
+  let offset = document.getElementById("desplazar").value;
+  let string = document.getElementById("mensaje_cifrar").value.toUpperCase();
 
-    const resultado = decode(string, offset);
-    document.getElementById("mensaje_cifrado").value = resultado;
+  const resultado = decode(string, offset);
+  document.getElementById("mensaje_cifrado").value = resultado;
 }
 
 document.getElementById("inicio").addEventListener("click", mostrar);
-function mostrar(){
-    document.getElementById("principal").style.display="none";
-    document.getElementById("secundario").style.display="block";
+
+function mostrar() {
+  document.getElementById("principal").style.display = "none";
+  document.getElementById("secundario").style.display = "block";
 }
-
-
